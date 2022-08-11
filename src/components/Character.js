b39ba7bd07d6344cf3c3b89b5662979929f32f9e
Will <willyknapp@gmail.com>
2022-08-11 00:15:50 -0400

    Initial commit of spritesheet with basic animation css.
    I plan on breaking the spritesheet up into individual horizontal animations to avert the need to do vertical transformations to achieve different directions of walking. This will also bring the animation panes closer in line with the standing images provided in the free assets I found, meaning similar solutions will be used for presenting both images.

---
+++ src/components/Character.js
@@ -0,0 +1,20 @@
+import react, {useState} from "react";
+import "./css/character.css";
+import character from "./images/character/Spritesheet Walk.png"
+
+function Character(props) {
+
+
+
+  return (
+    <div className="Character-container">
+      <img
+        src={character}
+        alt="Character"
+        className="Character-sprite walking face_up_right"
+        />
+    </div>
+  )
+}
+
+export default Character;
